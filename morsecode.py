a66fb6cced68a3aad674f971e0e3615ae4bb041e
Hun <gnstmahs@gmail.com>
2021-01-21 23:04:00 +0900

    update

--- morsecode.py
+++ morsecode.py
@@ -84,7 +84,7 @@ def is_validated_english_sentence(user_input):
     """
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당 또는 필요에 따라 자유로운 수정
-    result = False if re.search('[_@#$%^&*()-+=[\]{}"\';:\|`~]|[0-9]', user_input) or len(re.sub('[.,!?]', '', user_input)) == 0 else True
+    result = False if re.search('[_@#$%^\&\*()\-\+=[\]{\}"\';:\|`~]|[0-9]', user_input) or len(re.sub('[.,!?]', '', user_input)) == 0 else True
     
     return result
     # ==================================
